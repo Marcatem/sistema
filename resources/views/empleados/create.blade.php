9bd12538fe61481dd417a646da72a43a5dfcb12b
Marcatem <eutiquio1000@gmail.com>
2021-05-28 23:34:38 -0500

    Creacion de un formulario desde el archivo create.blade.php

--- resources/views/empleados/create.blade.php
+++ resources/views/empleados/create.blade.php
@@ -1,2 +1,24 @@
 <!-- formulacion de creacion de empleados-->
-create
+<form action="" method="post" enctype="multipart/form-data">
+    <label for="Nombre">Nombre</label>
+    <input type="text" name="Nombre" id="Nombre">
+    <br>
+    <br>
+    <label for="ApellidoPaterno">ApellidoPaterno</label>
+    <input type="text" name="ApellidoPaterno" id="ApellidoPaterno">
+    <br>
+    <br>
+    <label for="ApellidoMaterno">ApellidoMaterno</label>
+    <input type="text" name="ApellidoMaterno" id="ApellidoMaterno">
+    <br>
+    <br>
+    <label for="Correo">Correo</label>
+    <input type="text" name="Correo" id="Correo">
+    <br>
+    <br>
+    <label for="Foto">Foto</label>
+    <input type="file" name="Foto" id="Foto">
+    <br>
+    <br>
+    <input type="submit" name="Enviar" id="Enviar">
+</form>
--- resources/views/empleados/form.blade.php
+++ resources/views/empleados/form.blade.php
@@ -1,2 +1,9 @@
 <!-- Formmulario que tendra datos en comun con create y edit-->
-form
+<form action="" method="post" enctype="multipart/form-data">
+<input type="text" name="Nombre">
+<input type="text" name="ApellidoPaterno">
+<input type="text" name="Apellido Materno">
+<input type="text" name="Correo ">
+<input type="file" name="Foto">
+<input type="submit" name="Enviar informacion">
+</form>
