8612f09a447669af28271b6ace289435aa74de7d
Marcatem <eutiquio1000@gmail.com>
2021-05-30 19:41:51 -0500

    mostrar fotografia del storage en pantalla

--- resources/views/empleados/form.blade.php
+++ resources/views/empleados/form.blade.php
@@ -1,23 +1,23 @@
 <!-- edicion de la tabla empleados-->
 <label for="Nombre">Nombre</label>
-    <input type="text" name="Nombre" id="Nombre" value="{{$empleado->Nombre}}">
-    <br>
-    <br>
-    <label for="ApellidoPaterno">ApellidoPaterno</label>
-    <input type="text" name="ApellidoPaterno" id="ApellidoPaterno" value="{{$empleado->ApellidoPaterno}}">
-    <br>
-    <br>
-    <label for="ApellidoMaterno">ApellidoMaterno</label>
-    <input type="text" name="ApellidoMaterno" id="ApellidoMaterno" value="{{$empleado->ApellidoMaterno}}">
-    <br>
-    <br>
-    <label for="Correo">Correo</label>
-    <input type="text" name="Correo" id="Correo" value="{{$empleado->Correo}}">
-    <br>
-    <br>
-    <label for="Foto">Foto</label>
-    {{$empleado->Foto}}
-    <input type="file" name="Foto" id="Foto" value="">
-    <br>
-    <br>
-    <input type="submit" value="Guardar datos">
+<input type="text" name="Nombre" id="Nombre" value="{{$empleado->Nombre}}">
+<br>
+<br>
+<label for="ApellidoPaterno">ApellidoPaterno</label>
+<input type="text" name="ApellidoPaterno" id="ApellidoPaterno" value="{{$empleado->ApellidoPaterno}}">
+<br>
+<br>
+<label for="ApellidoMaterno">ApellidoMaterno</label>
+<input type="text" name="ApellidoMaterno" id="ApellidoMaterno" value="{{$empleado->ApellidoMaterno}}">
+<br>
+<br>
+<label for="Correo">Correo</label>
+<input type="text" name="Correo" id="Correo" value="{{$empleado->Correo}}">
+<br>
+<br>
+<label for="Foto">Foto</label>
+<img src="{{asset('storage'). '/'.$empleado->Foto}}" alt="">
+<input type="file" name="Foto" id="Foto" value="">
+<br>
+<br>
+<input type="submit" value="Guardar datos">
